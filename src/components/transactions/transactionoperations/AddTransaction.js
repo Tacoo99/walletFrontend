8e7d27a94e599cb30f067ebeb430d4a38cd86d59
Wojtek <koziolwojciech99@gmail.com>
2022-06-22 16:26:28 +0200

    Naprawiono dodawanie transakcji

--- src/components/transactions/transactionoperations/AddTransaction.js
+++ src/components/transactions/transactionoperations/AddTransaction.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import {createTransaction} from '../../../actions/projectActions';
+import { createTransaction } from '../../../actions/projectActions';
 import { connect } from 'react-redux';
-import {getWallet,updateWallet} from '../../../actions/projectActions'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import classnames from 'classnames'
 
+import { getWallet, updateWallet } from '../../../actions/projectActions'
 
 
 class AddTransaction extends Component {
@@ -21,35 +20,35 @@ class AddTransaction extends Component {
 
 
             //Wallet
-            id:'',
+            id: '',
             name: '',
             accountNumber: '',
             descriptionWallet: '',
             priority: '',
-            currentBalance:'',
-            errors:'',
+            currentBalance: '',
+            errors: '',
 
             newBalance: ''
         }
     }
 
-    componentWillReceiveProps(nextProps){
-        if(nextProps.errors){
-            this.setState({errors:nextProps.errors})
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors })
         }
-        if(nextProps.wallet){
+        if (nextProps.wallet) {
             this.setState({
-                id:nextProps.wallet.id,
+                id: nextProps.wallet.id,
                 name: nextProps.wallet.name,
                 accountNumber: nextProps.wallet.accountNumber,
                 descriptionWallet: nextProps.wallet.description,
                 priority: nextProps.wallet.priority,
-                currentBalance:nextProps.wallet.currentBalance,
+                currentBalance: nextProps.wallet.currentBalance,
             })
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.getWallet(this.props.match.params.id)
     }
 
@@ -59,85 +58,77 @@ class AddTransaction extends Component {
         })
 
     }
+
+
     handleSubmit = (event) => {
 
-        let balance;
-        const notify = () => toast.error('Portfel nie jest pusty, przenieś najpierw swoje środki!', {
-            position: "top-center",
-            autoClose: 3500,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            });
+        let balance = 0;
 
-        let newTransaction = { 
-            amount: this.state.amount, 
-            description: this.state.description, 
+        let newTransaction = {
+            amount: this.state.amount,
+            description: this.state.description,
             type: this.state.type,
         }
-        
-        const updateWallet = {
-            id:this.state.id,
-            name: this.state.name,
-            accountNumber: this.state.accountNumber,
-            description: this.state.descriptionWallet,
-            currentBalance: balance,
-            priority: this.state.priority
+
+
+        const update = () => {
+            const updateWallet = {
+                id: this.state.id,
+                name: this.state.name,
+                accountNumber: this.state.accountNumber,
+                description: this.state.descriptionWallet,
+                currentBalance: balance,
+                priority: this.state.priority
+            }
+
+            this.props.updateWallet(this.state.id, updateWallet, this.props.history)
+            this.props.createTransaction(newTransaction, this.props.history, this.props.match.params.id);
+            event.preventDefault();
+
         }
 
-        if(this.state.type === '1'){
+        if (this.state.type === '1') {
             balance = parseInt(this.state.currentBalance) + parseInt(this.state.amount);
-         }
-         if(this.state.type === '2'){
-             balance = parseInt(this.state.currentBalance) - parseInt(this.state.amount);
-             if(balance < 0){
-                 { notify() }
-             }
-         }
-        this.props.updateWallet(this.state.id,updateWallet,this.props.history)
-        this.props.createTransaction(newTransaction,this.props.history,this.props.match.params.id);
-        event.preventDefault();
+            { update() }
+        }
+        if (this.state.type === '2') {
+            balance = parseInt(this.state.currentBalance) - parseInt(this.state.amount);
+            if (balance < 0) {
+                alert('Stan konta nie może być ujemny!')
+            }
+            else {
+                { update() }
+            }
+        }
+
+
     }
 
-   
     render() {
         let id = this.props.match.params.id;
         const { amount, description, type } = this.state;
 
-        
+
         return (
             <div className="add-PBI">
                 <div className="container">
                     <div className="row">
-                    <ToastContainer
-                            position="top-center"
-                            autoClose={5000}
-                            hideProgressBar={false}
-                            newestOnTop={false}
-                            closeOnClick
-                            rtl={false}
-                            pauseOnFocusLoss
-                            draggable
-                            pauseOnHover
-                        />
                         <div className="col-md-8 m-auto">
                             <Link to={`/transactions/${id}`} className="btn btn-light">
                                 Wróć
-                    </Link>
+                            </Link>
                             <h4 style={{
                                 marginBottom: '4%'
                             }}
-                            className="display-4 text-center">
+                                className="display-4 text-center">
                                 Dodaj nową transakcję</h4>
                             <form onSubmit={this.handleSubmit}>
                                 <div style={{
                                     marginBottom: '1%'
                                 }}
-                                className="form-group">
-                                    <input type="number" min="1" value={amount} onChange={event => this.changeHandler(event, "amount", false)} className="form-control form-control-lg" placeholder="Kwota" required />
-                                    
+                                    className="form-group">
+                                    <input type="number" min="1" value={amount} onChange={event => this.changeHandler(event, "amount", false)} className={classnames("form-control form-control-lg", { "is-invalid": this.state.errors.amount })} placeholder="Kwota" required />
+                                    <p className="text-danger">{this.state.errors.amount}</p>
                                 </div>
                                 <div className="form-group">
                                     <textarea value={description} onChange={event => this.changeHandler(event, "description", false)} className="form-control form-control-lg" placeholder="Opis" required ></textarea>
@@ -147,7 +138,7 @@ class AddTransaction extends Component {
                                         marginTop: '1%',
                                         marginRight: '1%'
                                     }}
-                                    htmlFor="exampleFormControlTextarea1">Typ transakcji:</label>
+                                        htmlFor="exampleFormControlTextarea1">Typ transakcji:</label>
                                     <div className="form-check form-check-inline">
                                         <input checked={type === '1'} className="form-check-input" type="radio" id="income" onChange={event => this.changeHandler(event, "type", false)} value="1" />
                                         <label className="form-check-label" htmlFor="income">Dodaj</label>
@@ -168,9 +159,9 @@ class AddTransaction extends Component {
     }
 }
 
-const mapStateToProps = (state) =>({
-    errors:state.errors,
-    wallet:state.wallet.wallet
+const mapStateToProps = (state) => ({
+    errors: state.errors,
+    wallet: state.wallet.wallet
 })
 
-export default connect(mapStateToProps,{createTransaction, getWallet, updateWallet})(AddTransaction)
+export default connect(mapStateToProps, { createTransaction, getWallet, updateWallet })(AddTransaction)
